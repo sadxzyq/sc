56bae71e008970f409097b0e1d8cd464d18e25d4
sadxzyq <118393652+sadxzyq@users.noreply.github.com>
2022-12-28 12:40:36 +0700

    plugins/_assalamualaikum.js

--- plugin/_assalamualaikum.js
+++ plugin/_assalamualaikum.js
@@ -1,7 +1,8 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = ` _📚 ʙᴀᴄᴀ ʏᴀɴɢ ᴅɪʙᴀᴡᴀʜ ʏᴀ!_\n
+let info = ` waalaikumussalam warahmatullahi wabarakokatu
+_📚 ʙᴀᴄᴀ ʏᴀɴɢ ᴅɪʙᴀᴡᴀʜ ʏᴀ!_\n
 *"ᴏʀᴀɴɢ ʏᴀɴɢ ᴍᴇɴɢᴜᴄᴀᴘᴋᴀɴ ꜱᴀʟᴀᴍ ꜱᴇᴘᴇʀᴛɪ ɪɴɪ ᴍᴀᴋᴀ ɪᴀ ᴍᴇɴᴅᴀᴘᴀᴛᴋᴀɴ 30 ᴘᴀʜᴀʟᴀ, ᴋᴇᴍᴜᴅɪᴀɴ, ᴏʀᴀɴɢ ʏᴀɴɢ ᴅɪʜᴀᴅᴀᴘᴀɴ ᴀᴛᴀᴜ ᴍᴇɴᴅᴇɴɢᴀʀɴʏᴀ ᴍᴇᴍʙᴀʟᴀꜱ ᴅᴇɴɢᴀɴ ᴋᴀʟɪᴍᴀᴛ ʏᴀɴɢ ꜱᴀᴍᴀ ʏᴀɪᴛᴜ “ᴡᴀ'ᴀʟᴀɪᴋᴜᴍ ꜱᴀʟᴀᴍ ᴡᴀʀᴀʜᴍᴀᴛᴜʟʟᴀʜɪ ᴡᴀʙᴀʀᴀᴋᴀᴛᴜʜ” ᴀᴛᴀᴜ ᴅɪᴛᴀᴍʙᴀʜ ᴅᴇɴɢᴀɴ ʏᴀɴɢ ʟᴀɪɴ (ᴡᴀʀɪᴅʜᴡᴀᴀɴᴀ). ᴀʀᴛɪɴʏᴀ ꜱᴇʟᴀɪɴ ᴅᴀʀɪᴘᴀᴅᴀ ᴅᴏ'ᴀ ꜱᴇʟᴀᴍᴀᴛ ᴊᴜɢᴀ ᴍᴇᴍɪɴᴛᴀ ᴘᴀᴅᴀ ᴀʟʟᴀʜ ꜱᴡᴛ"* `
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 
